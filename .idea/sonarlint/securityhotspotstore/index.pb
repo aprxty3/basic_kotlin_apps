1ae98022b5982731911d1b420d5656deb9b8d03c
aji prasetyo <ajiprxty3@gmail.com>
2024-06-13 16:57:23 +0700

    create function

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,9 @@
+
+h
+8app/src/main/java/com/example/basic_apps/MainActivity.kt,8/2/82fa05cb560d7628949106bf929069d273da410a
+Å
+Q.idea/sonarlint/securityhotspotstore/8/2/82fa05cb560d7628949106bf929069d273da410a,4/5/459ac1e24ea92db53443718a7ceb4a2ddb8d6cd2
+]
+-.idea/sonarlint/securityhotspotstore/index.pb,8/8/88a9255124c95bdc913197c120a8d560edc59c8e
+S
+#.idea/sonarlint/issuestore/index.pb,9/f/9fe84ebb15faf917b7def6236dba604453cc61e0
--- app/src/main/java/com/example/basic_apps/MainActivity.kt
+++ app/src/main/java/com/example/basic_apps/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.example.basic_apps
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.EditText
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var etTinggi: EditText
+    private lateinit var etLebar: EditText
+    private lateinit var etPanjang: EditText
+    private lateinit var etCalculate: Button
+    private lateinit var etResult: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        etTinggi = findViewById(R.id.etTinggi)
+        etLebar = findViewById(R.id.etLebar)
+        etPanjang = findViewById(R.id.etPanjang)
+        etCalculate = findViewById(R.id.etCalculate)
+        etResult = findViewById(R.id.etResult)
+        etCalculate.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        if (v?.id == R.id.etCalculate) {
+            val inputPanjang = etPanjang.text.toString().trim()
+            val inputLebar = etLebar.text.toString().trim()
+            val inputTinggi = etTinggi.text.toString().trim()
+            val volume = inputLebar.toDouble() * inputPanjang.toDouble() * inputTinggi.toDouble()
+            etResult.text = volume.toString()
+        }
     }
 }
