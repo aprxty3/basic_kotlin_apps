33f4b3e0b1eba75ecb608441c57d50f1209c595e
aji prasetyo <ajiprxty3@gmail.com>
2024-06-21 22:09:56 +0700

    style and theme

---
+++ app/src/main/java/com/example/basic_apps/viewgroup_course/ConstraintLayout.kt
@@ -0,0 +1,12 @@
+package com.example.basic_apps.viewgroup_course
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.basic_apps.R
+
+class ConstraintLayout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constraint_layout)
+    }
+}
