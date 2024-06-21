e460c76fabc52c2cc378a05644ff658c71934e52
aji prasetyo <ajiprxty3@gmail.com>
2024-06-21 23:33:07 +0700

    implement style and themes on view

---
+++ app/src/main/java/com/example/basic_apps/styletheme/StyleThemeOne.kt
@@ -0,0 +1,12 @@
+package com.example.basic_apps.styletheme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.basic_apps.R
+
+class StyleThemeOne : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_style_theme_one)
+    }
+}
